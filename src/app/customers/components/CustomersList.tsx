7d6ba39fa50108e61efb13d2ff4846cc4a97ab9c
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-11-04 11:24:36 -0400

    refactor: split page.tsx (server) and CustomersList (client)

---
+++ src/app/customers/components/CustomersList.tsx
@@ -0,0 +1,63 @@
+import { Customer } from "@/api/customers";
+import { Button } from "@/components/ui/button";
+
+interface CustomersListProps {
+  customers: Customer[];
+}
+
+export default function CustomersList({ customers }: CustomersListProps) {
+  return (
+    <div>
+      <ul className="space-y-2 md:mx-20 lg:mx-60">
+        {customers.map((customer) => (
+          <li
+            key={customer.id}
+            className="border rounded-lg p-3 hover:bg-gray-50 transition flex flex-col gap-5 min-[480px]:flex-row items-center justify-between"
+          >
+            <div className="w-full flex flex-col items-center min-[480px]:items-start">
+              <p className="font-bold text-[12px] text-[var(--blue-primary)]">
+                {customer.name}
+              </p>
+
+              {customer.email ? (
+                <p className="text-[12px] text-[var(--gray-200)]">
+                  {customer.email}
+                </p>
+              ) : (
+                <p className="text-[12px] text-[var(--danger-secondary)]">
+                  E-mail não cadastrado no sistema
+                </p>
+              )}
+
+              {customer.cellphone ? (
+                <p className="text-[12px] text-[var(--gray-200)]">
+                  {customer.cellphone}
+                </p>
+              ) : (
+                <p className="text-[12px] text-[var(--danger-secondary)]">
+                  Telefone não cadastrado sistema
+                </p>
+              )}
+
+              {customer.street ? (
+                <p className="text-[12px] text-[var(--gray-200)]">
+                  {customer.street}
+                </p>
+              ) : (
+                <p className="text-[12px] text-[var(--danger-secondary)]">
+                  Endereço não cadastrado sistema
+                </p>
+              )}
+            </div>
+
+            <div>
+              <Button className="cursor-pointer bg-[var(--blue-secondary)]  hover:bg-[var(--blue-primary)]">
+                Gerar Contrato
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
