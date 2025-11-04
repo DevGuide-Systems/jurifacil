1575a4d241657f72dd470b1ca6025e5570846821
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-11-04 11:24:56 -0400

    feat: create a home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,16 @@
-import { getCustomers } from "@/api/customers";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-export default async function CustomersPage() {
-  const customers = await getCustomers();
-
+export default function Page() {
   return (
-    <main className="flex flex-col p-5 gap-6">
-      <div className=" flex flex-col items-start sm:items-center">
-        <h1 className="font-bold text-2xl text-[var(--blue-primary)]">
-          Clientes Cadastrados
-        </h1>
-        <p className="font-light text-[var(--blue-secondary)]">
-          Lista de clientes cadastrados no sistema ADVBOX
-        </p>
-      </div>
-
-      <ul className="space-y-2 md:mx-20 lg:mx-60">
-        {customers.map((customers) => (
-          <li
-            key={customers.id}
-            className="border rounded-lg p-3 hover:bg-gray-50 transition flex flex-row items-center justify-between"
-          >
-            <div>
-              <p className="font-bold text-[14px] text-[var(--blue-primary)]">
-                {customers.name}
-              </p>
-
-              {customers.email ? (
-                <p className="text-[12px] text-[var(--gray-200)]">
-                  {customers.email}
-                </p>
-              ) : (
-                <p className="text-[12px] text-[var(--danger-secondary)]">
-                  E-mail não cadastrado no sistema
-                </p>
-              )}
-
-              {customers.cellphone ? (
-                <p className="text-[12px] text-[var(--gray-200)]">
-                  {customers.cellphone}
-                </p>
-              ) : (
-                <p className="text-[12px] text-[var(--danger-secondary)]">
-                  Telefone não cadastrado sistema
-                </p>
-              )}
-            </div>
-
-            <div>
-              <Button className="cursor-pointer bg-[var(--blue-secondary)]  hover:bg-[var(--blue-primary)]">Gerar Contrato</Button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="w-full flex flex-col items-center mt-60 p-6 gap-2">
+      <h1 className="font-bold text-2xl text-[var(--blue-primary)]">Bem-vindo ao <span className="text-[var(--blue-secondary)]">JuriFácil</span></h1>
+      <p className="font-bold text-[12px] text-[var(--blue-secondary)] sm:text-[14px]">
+        Gere contratos de forma rápida e eficiente.
+      </p>
+      <Button className="cursor-pointer bg-[var(--blue-secondary)]  hover:bg-[var(--blue-primary)]">
+        <Link href="/customers">Gerar Contratos</Link>{" "}
+      </Button>
+    </div>
   );
 }
