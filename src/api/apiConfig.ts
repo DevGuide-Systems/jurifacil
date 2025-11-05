07776e5e3f929d04cae796a4e6b2366906add99e
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-11-05 09:02:11 -0400

    refactor: adjust folders

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_ADVBOX_API_URL, 
   headers: {
-    Authorization: `Bearer ${process.env.ADVBOX_API_KEY}`, // token privado
+    Authorization: `Bearer ${process.env.ADVBOX_API_KEY}`,
     "Content-Type": "application/json",
   },
 });
