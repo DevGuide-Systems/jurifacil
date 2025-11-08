c11f564cf2d3aa06593b6bff333700fac9e4aad0
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-11-08 12:31:23 -0400

    feat: add functionality to send generated contracts to Dropbox

---
+++ src/app/api/contracts/dropbox/route.ts
@@ -0,0 +1,116 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
+import { getCustomerById } from "@/api/customers/customers";
+import { Dropbox } from "dropbox";
+
+const dbx = new Dropbox({
+  accessToken: process.env.DROPBOX_ACCESS_TOKEN,
+  fetch,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { template, customerId } = await req.json();
+
+    if (!template || !customerId) {
+      return NextResponse.json(
+        { error: "Template e customerId são obrigatórios" },
+        { status: 400 }
+      );
+    }
+
+    const customer = await getCustomerById(customerId);
+
+    const gender = customer.gender || "";
+    let nationality = "BRASILEIRO";
+    if (gender.toUpperCase() === "F") {
+      nationality = "BRASILEIRA";
+    }
+
+    const templatePath = path.join(
+      process.cwd(),
+      "src",
+      "templates",
+      "documents",
+      template
+    );
+
+    const content = fs.readFileSync(templatePath);
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, {
+      paragraphLoop: true,
+      linebreaks: true,
+      syntax: {
+        allowUnopenedTag: true,
+      },
+      delimiters: {
+        start: "{{",
+        end: "}}",
+      },
+    });
+
+    doc.render({
+      name: customer.name || " {{name}} ",
+      identification: customer.identification || " {{identification}} ",
+      document: customer.document || " {{document}} ",
+      cellphone: customer.cellphone || " {{cellphone}} ",
+      gender: customer.gender || " {{gender}} ",
+      nationality,
+      civilstatus: customer.civil_status || " {{civilstatus}} ",
+      phone: customer.phone || " {{phone}} ",
+      email: customer.email || " {{email}} ",
+      occupation: customer.occupation || " {{occupation}} ",
+      street: customer.street || " {{street}} ",
+      postalcode: customer.postalcode || " {{postalcode}} ",
+      region: customer.region || " {{region}} ",
+      city: customer.city || " {{city}} ",
+      state: customer.state || " {{state}} ",
+      country: customer.country || " {{country}} ",
+      birthdate: customer.birthdate || " {{birthdate}} ",
+      numberctps: customer.number_ctps || " {{numberctps}} ",
+      numberpis: customer.number_pis || " {{numberpis}} ",
+      numbercid: customer.number_cid || " {{numbercid}} ",
+      notes: customer.notes || " {{notes}} ",
+      origin: customer.origin || " {{origin}} ",
+      created_at: customer.created_at || " {{created_at}} ",
+      date: new Date(Date.now()).toLocaleDateString("pt-BR", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }),
+    });
+
+    const buffer = doc.getZip().generate({ type: "nodebuffer" });
+
+    const templateName = path.basename(template, path.extname(template));
+    const customerName = customer.name.replace(/\s+/g, "_").toLowerCase();
+    const date = new Date().toISOString().split("T")[0].replace(/-/g, "");
+
+    const fileName = `${templateName}-${customerName}-${date}.docx`;
+
+    const dropboxPath = `/jurifacil/${fileName}`;
+    await dbx.filesUpload({
+      path: dropboxPath,
+      contents: buffer,
+      mode: { ".tag": "overwrite" },
+    });
+
+    return new NextResponse(new Uint8Array(buffer), {
+      status: 200,
+      headers: {
+        "Content-Disposition": `attachment; filename="${fileName}"`,
+        "Content-Type":
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      },
+    });
+  } catch (err: unknown) {
+    console.log("Erro ao gerar contrato:", err);
+    return NextResponse.json({
+      message: "Contrato enviado para Dropbox!",
+      link: null,
+    });
+  }
+}
