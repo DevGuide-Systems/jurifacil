9e884ca83743340452c7478746921b6eb8f0b476
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-11-08 12:34:50 -0400

    refactor: remove Clerk authentication integration

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,5 @@
 import { ElementType } from "react";
 import Image, { StaticImageData } from "next/image";
-import { auth } from "@clerk/nextjs/server";
-import LoginButton from "../LoginButton";
-import LogoutButton from "../LogoutButton";
-
 interface HeaderProps {
   text: string;
   icon: ElementType;
@@ -11,8 +7,6 @@ interface HeaderProps {
 }
 
 export default async function Header({ icon: Icon, text, image }: HeaderProps) {
-  const { userId } = await auth();
-
   return (
     <header className="flex flex-row justify-between items-center py-5 px-7 min-[768px]:px-15">
       <div className="">
@@ -21,7 +15,6 @@ export default async function Header({ icon: Icon, text, image }: HeaderProps) {
       <div className="flex flex-row gap-5 text-[var(--blue-secondary)] font-bold items-center">
         <Icon />
         {text}
-        {userId ? <LogoutButton /> : <LoginButton />}
       </div>
     </header>
   );
