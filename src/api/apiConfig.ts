5827d6404b8c0dcc28f9ecc9c27adfc099b55842
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-12-19 16:56:40 -0400

    refactor: refactor api organization and logic

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ADVBOX_API_URL, 
+  baseURL: process.env.NEXT_PUBLIC_ADVBOX_API_URL,
   headers: {
     Authorization: `Bearer ${process.env.ADVBOX_API_KEY}`,
     "Content-Type": "application/json",
--- src/api/customers/customers.ts
+++ src/api/customers/customers.ts
@@ -1,30 +1,5 @@
 import api from "../apiConfig";
-
-export interface Customer {
-  id: string;
-  name: string;
-  identification: string;
-  document: string;
-  cellphone: string;
-  gender: string;
-  civil_status: string;
-  phone: string;
-  email: string;
-  occupation: string;
-  street: string;
-  postalcode: string;
-  region: string;
-  city: string;
-  state: string;
-  country: string;
-  birthdate: string;
-  number_ctps: string;
-  number_pis: string;
-  number_cid: string;
-  notes: string;
-  origin: string;
-  created_at: string;
-}
+import { Customer } from "@/types/customer";
 
 export async function getCustomers(): Promise<Customer[]> {
   const response = await api.get("/customers");
